80babf0442ac5bef3f0fe03d3ee1d1c5e6ecf42c
wluyima <wyclif@openmrs.com>
2012-12-19 20:26:42 -0500

    Added javadocs to MockRegistrationEventListener - RC-4

--- api/src/test/java/org/openmrs/module/registrationcore/api/MockRegistrationEventListener.java
+++ api/src/test/java/org/openmrs/module/registrationcore/api/MockRegistrationEventListener.java
@@ -1,3 +1,16 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.registrationcore.api;
 
 import java.util.List;
@@ -10,8 +23,8 @@ import junit.framework.Assert;
 import org.openmrs.event.MockEventListener;
 
 /**
- *
- *
+ * Subclass of {@link MockEventListener} that extracts the patient registration pay load for testing
+ * purposes
  */
 public class MockRegistrationEventListener extends MockEventListener {
 	
