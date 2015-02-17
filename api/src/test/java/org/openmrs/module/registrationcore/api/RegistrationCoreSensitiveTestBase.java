d11ce28cfc1d68839788a9c2ea0447f1f65ad6c4
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-02-17 10:48:49 +0100

    Renamed test application context

--- api/src/test/java/org/openmrs/module/registrationcore/api/RegistrationCoreSensitiveTestBase.java
+++ api/src/test/java/org/openmrs/module/registrationcore/api/RegistrationCoreSensitiveTestBase.java
@@ -3,7 +3,7 @@ package org.openmrs.module.registrationcore.api;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(locations={"classpath*:applicationContext-service.xml","classpath*:moduleApplicationContext.xml", "classpath:TestingApplicationContext.xml"}, inheritLocations=false)
+@ContextConfiguration(locations={"classpath*:applicationContext-service.xml","classpath*:moduleApplicationContext.xml", "classpath:registrationcore-test-applicationContext.xml"}, inheritLocations=false)
 public class RegistrationCoreSensitiveTestBase extends BaseModuleContextSensitiveTest {
 	
 }
