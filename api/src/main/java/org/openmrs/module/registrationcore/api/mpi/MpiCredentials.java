c00798de1c6015f053666ff4b0d520a0a293ea47
approce <seateq64@gmail.com>
2015-06-19 09:41:21 +0300

    Use MpiCredentials as xml-pattern for querying to get token. Add verifying token exist in openEmpi service. @djazayeri.

--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/MpiCredentials.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/MpiCredentials.java
@@ -1,5 +1,8 @@
 package org.openmrs.module.registrationcore.api.mpi;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "authenticationRequest")
 public class MpiCredentials {
 
     private String username;
@@ -8,24 +11,35 @@ public class MpiCredentials {
 
     private String token;
 
+    public MpiCredentials() {
+    }
+
     public MpiCredentials(String username, String password) {
         this.username = username;
         this.password = password;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
     }
 
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getToken() {
         return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/OpenEmpiPatientSearchAlgorithm.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/OpenEmpiPatientSearchAlgorithm.java
@@ -1,9 +1,14 @@
 package org.openmrs.module.registrationcore.api.mpi;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 import org.openmrs.Patient;
 import org.openmrs.module.registrationcore.api.search.PatientAndMatchQuality;
 import org.springframework.stereotype.Service;
 
+import javax.ws.rs.core.UriBuilder;
 import java.util.List;
 import java.util.Map;
 
@@ -12,8 +17,30 @@ public class OpenEmpiPatientSearchAlgorithm implements MpiSimilarPatientSearchAl
 
     private MpiCredentials credentials;
 
+    private String SERVER_URL = "http://178.62.213.106:8080";
+    private String AUTHENTICATION_URL = "/openempi-admin/openempi-ws-rest/security-resource/authenticate";
+
     @Override
-    public List<PatientAndMatchQuality> findSimilarPatients(Patient patient, Map<String, Object> otherDataPoints, Double cutoff, Integer maxResults) {
+    public List<PatientAndMatchQuality> findSimilarPatients(Patient patient,
+                                                            Map<String, Object> otherDataPoints,
+                                                            Double cutoff, Integer maxResults) {
+        if (credentials.getToken() == null) {
+            processAuthentication();
+        }
         return null;
+    }
+
+
+    private void processAuthentication() {
+        ClientConfig config = new DefaultClientConfig();
+        Client client = Client.create(config);
+        WebResource service = client.resource(UriBuilder.fromUri(SERVER_URL + AUTHENTICATION_URL).build());
+
+        String token = service.put(String.class, credentials);
+        credentials.setToken(token);
+    }
+
+    public void setCredentials(MpiCredentials credentials) {
+        this.credentials = credentials;
     }
 }
