cab4648d0f9f596f3ebfe6946e07723bc8c0f9b2
approce <seateq64@gmail.com>
2015-06-20 15:18:45 +0300

    Create class with variables for OpenEMPI.

---
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/OpenEmpiVariables.java
@@ -0,0 +1,23 @@
+package org.openmrs.module.registrationcore.api.mpi.openempi;
+
+public class OpenEmpiVariables {
+
+    static final String TOKEN_HEADER_KEY = "OPENEMPI_SESSION_KEY";
+
+    //TODO move to property
+    private static final String SERVER_URL = "http://178.62.213.106:8080/";
+
+    private static final String REST_URL = "openempi-admin/openempi-ws-rest/";
+
+    private static final String AUTHENTICATION_URL = "security-resource/authenticate";
+
+    private static final String FIND_PATIENTS_URL = "person-query-resource/findPersonsByAttributes";
+
+    static String getAuthenticationUrl() {
+        return SERVER_URL + REST_URL + AUTHENTICATION_URL;
+    }
+
+    static String getFindPatientsUrl() {
+        return SERVER_URL + REST_URL + FIND_PATIENTS_URL;
+    }
+}
