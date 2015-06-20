9a0b3ffb52eca8cc5f0001a5578771c03d8ba26f
approce <seateq64@gmail.com>
2015-06-20 15:19:24 +0300

    Create class (xml representation) for querying matching patients.

---
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/OpenEmpiPatientQuery.java
@@ -0,0 +1,58 @@
+package org.openmrs.module.registrationcore.api.mpi.openempi;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
+@XmlRootElement(name = "person")
+public class OpenEmpiPatientQuery {
+
+    private String familyName;
+
+    private String givenName;
+
+    private String middleName;
+
+    private String genderDescription;
+
+    private Date birthDate; //TODO: String?
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getGenderDescription() {
+        return genderDescription;
+    }
+
+    public void setGenderDescription(String genderDescription) {
+        this.genderDescription = genderDescription;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
