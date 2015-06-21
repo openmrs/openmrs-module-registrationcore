2caa2af0ee623ebffede0b468dc51929e477ba72
approce <seateq64@gmail.com>
2015-06-21 19:10:39 +0300

    Add method for converting from mpiPatient to openMrsPatient.

---
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/PatientQueryMapper.java
@@ -0,0 +1,36 @@
+package org.openmrs.module.registrationcore.api.mpi.openempi;
+
+import org.openmrs.Patient;
+import org.openmrs.PersonName;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+public class PatientQueryMapper {
+
+    public static OpenEmpiPatientQuery convert(Patient patient) {
+        OpenEmpiPatientQuery patientQuery = new OpenEmpiPatientQuery();
+
+        patientQuery.setFamilyName(patient.getFamilyName());
+        patientQuery.setGivenName(patient.getGivenName());
+        patientQuery.setMiddleName(patient.getMiddleName());
+        patientQuery.setDateOfBirth(patient.getBirthdate());
+        patientQuery.setGenderDescription(patient.getGender());
+
+        return patientQuery;
+    }
+
+    public static Patient convert(OpenEmpiPatientQuery patientQuery) {
+        Patient patient = new Patient();
+
+        PersonName names = new PersonName();
+        names.setFamilyName(patientQuery.getFamilyName());
+        names.setGivenName(patientQuery.getGivenName());
+        names.setMiddleName(patientQuery.getMiddleName());
+        patient.setNames(new HashSet<PersonName>(Collections.singleton(names)));
+        patient.setBirthdate(patientQuery.getDateOfBirth());
+        patient.setGender(patientQuery.getGenderDescription());
+
+        return patient;
+    }
+}
