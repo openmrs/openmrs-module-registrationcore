597ce873f5432b0c7ab6ca8793d5c614f6af175d
approce <seateq64@gmail.com>
2015-06-26 08:49:08 +0300

    Change rest lib from "Jersey" to "Spring RestTemple" in reason of incorrect XML marshalling in Jersey lib.

--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/OpenEmpiExactPatientSearchAlgorithm.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/OpenEmpiExactPatientSearchAlgorithm.java
@@ -1,70 +1,51 @@
 package org.openmrs.module.registrationcore.api.mpi.openempi;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.GenericType;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
 import org.openmrs.Patient;
 import org.openmrs.module.registrationcore.api.mpi.MpiCredentials;
 import org.openmrs.module.registrationcore.api.mpi.MpiSimilarPatientSearchAlgorithm;
 import org.openmrs.module.registrationcore.api.search.PatientAndMatchQuality;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
-import javax.ws.rs.core.UriBuilder;
+import javax.naming.AuthenticationException;
 import java.util.*;
 
 @Service("registrationcore.OpenEmpiExactPatientSearchAlgorithm")
 public class OpenEmpiExactPatientSearchAlgorithm implements MpiSimilarPatientSearchAlgorithm {
 
+    String token = "F58C465C3045B63D7542F2D3BE9BA9DF";
     private MpiCredentials credentials;
-
-    private Client client = Client.create(new DefaultClientConfig());
 
     @Override
     public List<PatientAndMatchQuality> findSimilarPatients(Patient patient,
                                                             Map<String, Object> otherDataPoints,
                                                             Double cutoff, Integer maxResults) {
-        this.setCredentials(new MpiCredentials("admin", "admin"));
-        if (credentials.getToken() == null) {
-            processAuthentication();
+        //TODO mocking. Should be removed.
+        credentials = new MpiCredentials("admin", "admin");
+
+        List<PatientAndMatchQuality> result;
+        try {
+            if (!isAuthenticated()) {
+                processAuthentication();
+            }
+            List<OpenEmpiPatientQuery> mpiSimilarPatients = getMpiSimilarPatients(patient);
+            subtractMpiPatients(mpiSimilarPatients, maxResults);
+            result = convertMpiPatients(mpiSimilarPatients, cutoff);
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+            result = Collections.emptyList();
         }
-        return findPatients(patient, cutoff, maxResults);
+        return result;
     }
 
-    private void processAuthentication() {
-        WebResource service = client.resource(
-                UriBuilder.fromUri(OpenEmpiVariables.getAuthenticationUrl()).build());
-
-        String token = service.put(String.class, credentials);
-        credentials.setToken(token);
-    }
-
-    private List<PatientAndMatchQuality> findPatients(Patient patient, Double cutoff, Integer maxResults) {
-        OpenEmpiPatientQuery patientQuery = getQuery(patient);
-
-        WebResource.Builder service = getRestBuilder();
-
-        List<OpenEmpiPatientQuery> mpiPatients = getMatchedPatients(patientQuery, service, maxResults);
-
-        return convertMatchedPatients(mpiPatients, cutoff);
-    }
-
-    private OpenEmpiPatientQuery getQuery(Patient patient) {
-        return PatientQueryMapper.convert(patient);
-    }
-
-    private WebResource.Builder getRestBuilder() {
-        return client.resource(UriBuilder.fromUri(OpenEmpiVariables.getFindPatientsUrl()).build())
-                .header(OpenEmpiVariables.TOKEN_HEADER_KEY, credentials.getToken());
-    }
-
-    private List<OpenEmpiPatientQuery> getMatchedPatients(OpenEmpiPatientQuery patientQuery,
-                                                          WebResource.Builder service, int maxResults) {
-        return service.post(new GenericType<List<OpenEmpiPatientQuery>>() {}, patientQuery);
-    }
-
-    private List<PatientAndMatchQuality> convertMatchedPatients(List<OpenEmpiPatientQuery> mpiPatients,
-                                                                Double cutoff) {
+    private List<PatientAndMatchQuality> convertMpiPatients(List<OpenEmpiPatientQuery> mpiPatients,
+                                                            Double cutoff) {
         List<String> matchedFields = Arrays.asList("personName", "gender", "birthdate");
         List<PatientAndMatchQuality> result = new LinkedList<PatientAndMatchQuality>();
         for (OpenEmpiPatientQuery mpiPatient : mpiPatients) {
@@ -76,7 +57,56 @@ public class OpenEmpiExactPatientSearchAlgorithm implements MpiSimilarPatientSea
         return result;
     }
 
-    public void setCredentials(MpiCredentials credentials) {
-        this.credentials = credentials;
+    private boolean isAuthenticated() {
+        return credentials.getToken() != null;
+    }
+
+    private void subtractMpiPatients(List<OpenEmpiPatientQuery> mpiSimilarPatients, Integer maxResults) {
+        if (mpiSimilarPatients.size() > maxResults) {
+            mpiSimilarPatients = mpiSimilarPatients.subList(0, maxResults);
+        }
+    }
+
+    private void processAuthentication() throws AuthenticationException {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> token = restTemplate
+                .exchange(OpenEmpiVariables.getAuthenticationUrl(),
+                        HttpMethod.PUT, new HttpEntity<MpiCredentials>(credentials), String.class);
+        String tokenValue = token.getBody();
+        if (tokenValue != null) {
+            credentials.setToken(tokenValue);
+        } else {
+            throw new AuthenticationException("Can't perform authentication");
+        }
+    }
+
+    private List<OpenEmpiPatientQuery> getMpiSimilarPatients(Patient patient) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<OpenEmpiPatientQuery> request = getHttpEntityRequest(restTemplate, patient);
+
+        OpenEmpiPeopleWrapper people = restTemplate.postForObject(OpenEmpiVariables.getFindPatientsUrl(),
+                request, OpenEmpiPeopleWrapper.class);
+
+        return people.getPeople();
+    }
+
+    private HttpEntity<OpenEmpiPatientQuery> getHttpEntityRequest(RestTemplate restTemplate, Patient patient) {
+        MultiValueMap<String, String> headers = getAuthenticationHeader(restTemplate);
+        OpenEmpiPatientQuery query = getPatientSimilarQuery(patient);
+
+        return new HttpEntity<OpenEmpiPatientQuery>(query, headers);
+    }
+
+    private MultiValueMap<String, String> getAuthenticationHeader(RestTemplate restTemplate) {
+        restTemplate.getMessageConverters().add(new MappingJacksonHttpMessageConverter());
+
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+        headers.add(OpenEmpiVariables.TOKEN_HEADER_KEY, credentials.getToken());
+
+        return headers;
+    }
+
+    private OpenEmpiPatientQuery getPatientSimilarQuery(Patient patient) {
+        return PatientQueryMapper.convert(patient);
     }
 }
