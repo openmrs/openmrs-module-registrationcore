65bbb8d9cc4b4a6b22e1a7e4937472bc876603d9
approce <seateq64@gmail.com>
2015-07-01 23:28:27 +0300

    Add url to probablistic search to OpenEMPI variables. Rename existing variables. Add TODO to change actual url of probablistic search. (Use probablistic search instead of exact in future).

--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/OpenEmpiVariables.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/OpenEmpiVariables.java
@@ -11,13 +11,20 @@ public class OpenEmpiVariables {
 
     private static final String AUTHENTICATION_URL = "security-resource/authenticate";
 
-    private static final String FIND_PATIENTS_URL = "person-query-resource/findPersonsByAttributes";
+    private static final String FIND_EXACT_PATIENTS_URL = "person-query-resource/findPersonsByAttributes";
+
+    //TODO should be changed for Probablistic match.
+    private static final String FIND_PROBABLISTIC_PATIENTS_URL = "person-query-resource/findPersonsByAttributes";
 
     static String getAuthenticationUrl() {
         return SERVER_URL + REST_URL + AUTHENTICATION_URL;
     }
 
-    static String getFindPatientsUrl() {
-        return SERVER_URL + REST_URL + FIND_PATIENTS_URL;
+    static String getExactPatientsUrl() {
+        return SERVER_URL + REST_URL + FIND_EXACT_PATIENTS_URL;
+    }
+
+    static String getProbablisticPatientsUrl() {
+        return SERVER_URL + REST_URL + FIND_PROBABLISTIC_PATIENTS_URL;
     }
 }
