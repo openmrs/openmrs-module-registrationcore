a720d4f5e76f31e34737cbb3ed3076e97345e6c5
approce <seateq64@gmail.com>
2015-07-14 09:18:46 +0300

    Mark RestQueryCreator as Service.

--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/RestQueryCreator.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/RestQueryCreator.java
@@ -5,11 +5,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 import java.util.List;
 
+@Service
 public class RestQueryCreator {
 
     private MpiCredentials credentials;
