675187d5a87f5fe2bef4ce3193dff0a614048729
approce <seateq64@gmail.com>
2015-07-15 22:17:32 +0300

    Remove redundant interface extending in MpIFacade. Perform validating authentication before each method delegation. @djazayeri actually this is one of the reasons why I decided to use Facade.

--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/common/MpiFacade.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/common/MpiFacade.java
@@ -1,5 +1,5 @@
 package org.openmrs.module.registrationcore.api.mpi.common;
 
-public interface MpiFacade extends MpiAuthenticator, MpiSimilarPatientSearchAlgorithm, MpiPatientImporter{
+public interface MpiFacade extends MpiSimilarPatientSearchAlgorithm, MpiPatientImporter {
 
 }
--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/common/MpiFacadeImpl.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/common/MpiFacadeImpl.java
@@ -19,24 +19,27 @@ public class MpiFacadeImpl implements MpiFacade {
     private MpiAuthenticator authenticator;
 
     @Override
-    public void performAuthentication() {
-        authenticator.performAuthentication();
-    }
-
-    @Override
     public MpiPatient importMpiPatient(String patientId) {
+        validateAuthentication();
         return patientImporter.importMpiPatient(patientId);
     }
 
     @Override
     public List<PatientAndMatchQuality> findSimilarPatients(Patient patient, Map<String, Object> otherDataPoints,
                                                             Double cutoff, Integer maxResults) {
+        validateAuthentication();
         return searchAlgorithm.findSimilarPatients(patient, otherDataPoints, cutoff, maxResults);
     }
 
     @Override
     public List<PatientAndMatchQuality> findPreciseSimilarPatients(Patient patient, Map<String, Object> otherDataPoints,
                                                                    Double cutoff, Integer maxResults) {
+        validateAuthentication();
         return searchAlgorithm.findPreciseSimilarPatients(patient, otherDataPoints, cutoff, maxResults);
+    }
+
+    private void validateAuthentication() {
+        if (!authenticator.isAuthenticated())
+            authenticator.performAuthentication();
     }
 }
