021b824fe30dbb1afcfe41c635072feb199c9d90
approce <seateq64@gmail.com>
2015-07-25 19:55:52 +0300

    Refactor OpenEmpiAuthentication service. Get credentials from GP. Validate them. Process authentication with MpiCredentials.

--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/OpenEmpiAuthenticator.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/OpenEmpiAuthenticator.java
@@ -1,7 +1,11 @@
 package org.openmrs.module.registrationcore.api.mpi.openempi;
 
+import org.apache.commons.lang.StringUtils;
 import org.openmrs.api.APIException;
+import org.openmrs.api.AdministrationService;
+import org.openmrs.module.registrationcore.RegistrationCoreConstants;
 import org.openmrs.module.registrationcore.api.mpi.common.MpiAuthenticator;
+import org.openmrs.module.registrationcore.api.mpi.common.MpiCredentials;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -10,20 +14,34 @@ public class OpenEmpiAuthenticator implements MpiAuthenticator {
     @Autowired
     @Qualifier("registrationcore.restQueryCreator")
     private RestQueryCreator queryCreator;
-    private boolean authenticated;
+    @Autowired
+    @Qualifier("adminService")
+    private AdministrationService adminService;
+    private String token;
 
     @Override
     public boolean isAuthenticated() {
-        return authenticated;
+        return token == null;
     }
 
     @Override
     public void performAuthentication() {
         try {
-            queryCreator.processAuthentication();
-            authenticated = true;
+            token = queryCreator.processAuthentication(getMpiCredentials());
         } catch (RuntimeException e) {
             throw new APIException("Exception while processing authentication to MPI server.");
         }
+    }
+
+    private MpiCredentials getMpiCredentials() {
+        String username = adminService.getGlobalProperty(RegistrationCoreConstants.GP_MPI_ACCESS_USERNAME);
+        String password = adminService.getGlobalProperty(RegistrationCoreConstants.GP_MPI_ACCESS_PASSWORD);
+
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
+            throw new APIException("Incorrect credentials are set for MPI server." +
+                    "(Username: \"" + username + "\", Password: \"" + password + "\")");
+        }
+
+        return new MpiCredentials(username, password);
     }
 }
