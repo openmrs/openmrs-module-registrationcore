770f2348671915baea205dc07552bdd0798d423c
approce <seateq64@gmail.com>
2015-07-27 12:39:40 +0300

    Refactor RestQueryCreator.

--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/RestQueryCreator.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/RestQueryCreator.java
@@ -1,5 +1,9 @@
 package org.openmrs.module.registrationcore.api.mpi.openempi;
 
+import org.apache.commons.lang.StringUtils;
+import org.openmrs.api.APIException;
+import org.openmrs.api.AdministrationService;
+import org.openmrs.module.registrationcore.RegistrationCoreConstants;
 import org.openmrs.module.registrationcore.api.mpi.common.MpiCredentials;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -10,14 +14,21 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Collections;
 import java.util.List;
 
+import static org.openmrs.module.registrationcore.api.mpi.openempi.OpenEmpiVariables.*;
+
 public class RestQueryCreator {
 
+    private AdministrationService adminService;
     private RestTemplate restTemplate = new RestTemplate();
+    private String serverUrl;
+
+    public void setAdminService(AdministrationService adminService) {
+        this.adminService = adminService;
+    }
 
     public String processAuthentication(MpiCredentials credentials) throws RuntimeException {
-        ResponseEntity<String> token = restTemplate
-                .exchange(OpenEmpiVariables.getAuthenticationUrl(),
-                        HttpMethod.PUT, new HttpEntity<MpiCredentials>(credentials), String.class);
+        ResponseEntity<String> token = restTemplate.exchange(createAuthenticationUrl(),
+                HttpMethod.PUT, new HttpEntity<MpiCredentials>(credentials), String.class);
         String tokenValue = token.getBody();
         if (tokenValue != null) {
             return tokenValue;
@@ -31,11 +42,8 @@ public class RestQueryCreator {
 
         HttpEntity<String> entity = new HttpEntity<String>(headers);
 
-        String findPatientByIdUrl = createGetPatientUrl(id);
-
-
-        ResponseEntity<OpenEmpiPatientQuery> person = restTemplate.exchange(findPatientByIdUrl, HttpMethod.GET,
-                entity, OpenEmpiPatientQuery.class);
+        ResponseEntity<OpenEmpiPatientQuery> person = restTemplate.exchange(createGetPatientUrl(id),
+                HttpMethod.GET, entity, OpenEmpiPatientQuery.class);
 
         return person.getBody();
     }
@@ -45,7 +53,8 @@ public class RestQueryCreator {
 
         HttpEntity<OpenEmpiPatientQuery> entity = new HttpEntity<OpenEmpiPatientQuery>(query, headers);
 
-        ResponseEntity<OpenEmpiPeopleWrapper> people = restTemplate.exchange(OpenEmpiVariables.getExactPatientsUrl(), HttpMethod.POST, entity, OpenEmpiPeopleWrapper.class);
+        ResponseEntity<OpenEmpiPeopleWrapper> people = restTemplate.exchange(createFindPreciseSimilarPatient(),
+                HttpMethod.POST, entity, OpenEmpiPeopleWrapper.class);
 
         return unwrapResult(people.getBody());
     }
@@ -56,11 +65,33 @@ public class RestQueryCreator {
         return headers;
     }
 
+    private String createAuthenticationUrl() {
+        return getServerUrl() + REST_URL + AUTHENTICATION_URL;
+    }
+
     private String createGetPatientUrl(String id) {
-        String url = OpenEmpiVariables.getFindPatientByIdUrl();
-        url += "?personId=";
-        url += id;
+        String url = getServerUrl() + REST_URL + PERSON_QUERY_URL + FIND_PATIENT_BY_ID_URL;
+        url += "?personId" + id;
         return url;
+    }
+
+    private String createFindPreciseSimilarPatient() {
+        return getServerUrl() + REST_URL + PERSON_QUERY_URL + FIND_EXACT_PATIENTS_URL;
+    }
+
+    private String getServerUrl() {
+        if (serverUrl == null) {
+            serverUrl = adminService.getGlobalProperty(RegistrationCoreConstants.GP_MPI_URL);
+            validateServerUrl();
+            serverUrl = "http://" + serverUrl;
+        }
+        return serverUrl;
+    }
+
+    private void validateServerUrl() {
+        if (StringUtils.isBlank(serverUrl))
+            throw new APIException("Mpi server credentials are not set. Please fill "
+                    + RegistrationCoreConstants.GP_MPI_URL + " property");
     }
 
     private List<OpenEmpiPatientQuery> unwrapResult(OpenEmpiPeopleWrapper people) {
