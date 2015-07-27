002fbb5a203858dd3f89edf2c83f42124c4beaef
approce <seateq64@gmail.com>
2015-07-27 16:07:02 +0300

    Refactor MpiFacadeImpl test. Make it unit test, not integration. Add tests for each method & each case.

--- api/src/test/java/org/openmrs/module/registrationcore/api/mpi/common/MpiFacadeImplTest.java
+++ api/src/test/java/org/openmrs/module/registrationcore/api/mpi/common/MpiFacadeImplTest.java
@@ -1,41 +1,111 @@
 package org.openmrs.module.registrationcore.api.mpi.common;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.openmrs.Patient;
-import org.openmrs.PersonName;
-import org.openmrs.module.registrationcore.api.RegistrationCoreSensitiveTestBase;
 import org.openmrs.module.registrationcore.api.search.PatientAndMatchQuality;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
-public class MpiFacadeImplTest extends RegistrationCoreSensitiveTestBase {
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
-    private static final String PATIENT_GIVEN_NAME = "Roman";
-    private static final String PATIENT_FAMILY_NAME = "Zayats";
-    @Autowired
-    private MpiFacade mpiFacade;
+public class MpiFacadeImplTest {
 
-    @Test
-    public void testPerformCorrectSearchSimilar() throws Exception {
-        Patient patient = createPatient();
-        List<PatientAndMatchQuality> similarPatients = mpiFacade.findSimilarPatients(patient, null, null, 10);
-        System.out.println(similarPatients.size());
+    private static final String PATIENT_ID = "13";
+
+    @InjectMocks private MpiFacade mpiFacade = new MpiFacadeImpl();
+    @Mock private MpiSimilarPatientSearchAlgorithm searchAlgorithm;
+    @Mock private MpiPatientImporter patientImporter;
+    @Mock private MpiAuthenticator authenticator;
+
+    @Mock private List<PatientAndMatchQuality> listPatients;
+    @Mock private Map<String, Object> otherDataPoints;
+    @Mock private Patient patient;
+    private Double cutoff = 3d;
+    private Integer maxResults = 10;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void testPerformCorrectImport() throws Exception {
-        Patient mpiPatient = mpiFacade.importMpiPatient("2");
-        System.out.println(mpiPatient.getFamilyName());
+    public void testImportMpiPatient() throws Exception {
+        mockAuthentication(false);
+        Patient mockPatient = mock(Patient.class);
+        when(patientImporter.importMpiPatient(PATIENT_ID)).thenReturn(mockPatient);
+
+        Patient patient = mpiFacade.importMpiPatient(PATIENT_ID);
+
+        verify(authenticator).performAuthentication();
+        verify(patientImporter).importMpiPatient(PATIENT_ID);
+        assertEquals(patient, mockPatient);
     }
 
-    private Patient createPatient() {
-        Patient patient = new Patient();
-        HashSet<PersonName> names = new HashSet<PersonName>();
-        PersonName name = new PersonName(PATIENT_GIVEN_NAME, null, PATIENT_FAMILY_NAME);
-        names.add(name);
-        patient.setNames(names);
-        return patient;
+    @Test
+    public void testImportDoNotPerformAuthenticationInCaseAuthenticated() throws Exception {
+        mockAuthentication(true);
+        Patient mockPatient = mock(Patient.class);
+        when(patientImporter.importMpiPatient(PATIENT_ID)).thenReturn(mockPatient);
+
+        mpiFacade.importMpiPatient(PATIENT_ID);
+
+        verify(authenticator, never()).performAuthentication();
+    }
+
+    @Test
+    public void testFindSimilarPatients() throws Exception {
+        mockAuthentication(false);
+        when(searchAlgorithm.findSimilarPatients(any(Patient.class), any(Map.class), any(Double.class), anyInt()))
+                .thenReturn(listPatients);
+
+        List<PatientAndMatchQuality> similarPatients = mpiFacade.findSimilarPatients(patient, otherDataPoints, cutoff, maxResults);
+        verify(authenticator).performAuthentication();
+        verify(searchAlgorithm).findSimilarPatients(patient, otherDataPoints, cutoff, maxResults);
+        assertEquals(similarPatients, listPatients);
+    }
+
+    @Test
+    public void testFindSimilarPatientsDoNotPerformAuthenticationInCaseAuthenticated() throws Exception {
+        mockAuthentication(true);
+        when(searchAlgorithm.findSimilarPatients(any(Patient.class), any(Map.class), any(Double.class), anyInt()))
+                .thenReturn(listPatients);
+
+        mpiFacade.findSimilarPatients(patient, otherDataPoints, cutoff, maxResults);
+
+        verify(authenticator, never()).performAuthentication();
+    }
+
+    @Test
+    public void testFindPreciseSimilarPatients() throws Exception {
+        mockAuthentication(false);
+        when(searchAlgorithm.findPreciseSimilarPatients(any(Patient.class), any(Map.class), any(Double.class), anyInt()))
+                .thenReturn(listPatients);
+
+        List<PatientAndMatchQuality> similarPatients = mpiFacade.findPreciseSimilarPatients(patient, otherDataPoints, cutoff, maxResults);
+        verify(authenticator).performAuthentication();
+        verify(searchAlgorithm).findPreciseSimilarPatients(patient, otherDataPoints, cutoff, maxResults);
+        assertEquals(similarPatients, listPatients);
+
+    }
+
+    @Test
+    public void testFindPreciseSimilarPatientsDoNotPerformAuthenticationInCaseAuthenticated() throws Exception {
+        mockAuthentication(true);
+        when(searchAlgorithm.findPreciseSimilarPatients(any(Patient.class), any(Map.class), any(Double.class), anyInt()))
+                .thenReturn(listPatients);
+
+        mpiFacade.findPreciseSimilarPatients(patient, otherDataPoints, cutoff, maxResults);
+
+        verify(authenticator, never()).performAuthentication();
+    }
+
+    private void mockAuthentication(boolean authenticated) {
+        when(authenticator.isAuthenticated()).thenReturn(authenticated);
     }
 }
