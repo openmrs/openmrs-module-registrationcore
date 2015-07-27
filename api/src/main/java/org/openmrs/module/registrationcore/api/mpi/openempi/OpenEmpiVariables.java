02312f23c41b4c9552a76aba7dc12fd235f3db6a
approce <seateq64@gmail.com>
2015-07-27 12:36:35 +0300

    Refactor OpenEmpiVariables. Make them package visible. Remove Server url (move it to GP). Remove method for creating urls.

--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/OpenEmpiVariables.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/OpenEmpiVariables.java
@@ -4,35 +4,17 @@ public class OpenEmpiVariables {
 
     static final String TOKEN_HEADER_KEY = "OPENEMPI_SESSION_KEY";
 
-    //TODO move to property
-    private static final String SERVER_URL = "http://188.166.56.70:8080/";
+    static final String REST_URL = "/openempi-ws-rest/";
 
-    private static final String REST_URL = "openempi-admin/openempi-ws-rest/";
+    static final String PERSON_QUERY_URL = "person-query-resource/";
 
-    private static final String PERSON_QUERY_URL = "person-query-resource/";
+    static final String AUTHENTICATION_URL = "security-resource/authenticate";
 
-    private static final String AUTHENTICATION_URL = "security-resource/authenticate";
-
-    private static final String FIND_EXACT_PATIENTS_URL = "findPersonsByAttributes";
+    static final String FIND_EXACT_PATIENTS_URL = "findPersonsByAttributes";
 
     //TODO should be changed for Probablistic match.
-    private static final String FIND_PROBABLISTIC_PATIENTS_URL = "findPersonsByAttributes";
+    static final String FIND_PROBABLISTIC_PATIENTS_URL = "findPersonsByAttributes";
 
-    private static final String FIND_PATIENT_BY_ID_URL = "loadPerson";
+    static final String FIND_PATIENT_BY_ID_URL = "loadPerson";
 
-    static String getAuthenticationUrl() {
-        return SERVER_URL + REST_URL + AUTHENTICATION_URL;
-    }
-
-    static String getExactPatientsUrl() {
-        return SERVER_URL + REST_URL + PERSON_QUERY_URL + FIND_EXACT_PATIENTS_URL;
-    }
-
-    static String getProbablisticPatientsUrl() {
-        return SERVER_URL + REST_URL + PERSON_QUERY_URL + FIND_PROBABLISTIC_PATIENTS_URL;
-    }
-
-    static String getFindPatientByIdUrl() {
-        return SERVER_URL + REST_URL + PERSON_QUERY_URL + FIND_PATIENT_BY_ID_URL;
-    }
 }
