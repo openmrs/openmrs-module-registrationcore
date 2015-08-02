62087623624675e98dba9fd824eb4f5e9b6f20e4
approce <seateq64@gmail.com>
2015-08-02 16:06:45 +0300

    Refactor ModuleProperties and MpiProperties, move common things to Abstract parent class. Leave only getters in MpiProperties.

--- api/src/main/java/org/openmrs/module/registrationcore/api/ModuleProperties.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/ModuleProperties.java
@@ -6,11 +6,37 @@ import org.openmrs.api.AdministrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public abstract class ModuleProperties {
+
+    private static final Map<String, String> PROPERTIES = new LinkedHashMap<String, String>();
 
     @Autowired
     @Qualifier("adminService")
     protected AdministrationService administrationService;
+
+    protected Integer getIntegerProperty(String propertyName) {
+        String propertyValue = getProperty(propertyName);
+        try {
+            return Integer.valueOf(propertyValue);
+        } catch (NumberFormatException e) {
+            throw new APIException("Incorrect property value. " + propertyValue + " cannot be cast to Integer");
+        }
+    }
+
+    protected String getProperty(String propertyName) {
+        if (PROPERTIES.get(propertyName) == null) {
+            initializeProperty(propertyName);
+        }
+        return PROPERTIES.get(propertyName);
+    }
+
+    private void initializeProperty(String propertyName) {
+        String propertyValue = readProperty(propertyName);
+        PROPERTIES.put(propertyName, propertyValue);
+    }
 
     protected String readProperty(String propertyName) {
         String propertyValue = administrationService.getGlobalProperty(propertyName);
--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/common/MpiProperties.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/common/MpiProperties.java
@@ -1,15 +1,9 @@
 package org.openmrs.module.registrationcore.api.mpi.common;
 
-import org.openmrs.api.APIException;
 import org.openmrs.module.registrationcore.RegistrationCoreConstants;
 import org.openmrs.module.registrationcore.api.ModuleProperties;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class MpiProperties extends ModuleProperties {
-
-    private static final Map<String, String> MPI_PROPERTIES = new LinkedHashMap<String, String>();
 
     public String getServerUrl() {
         String propertyName = RegistrationCoreConstants.GP_MPI_URL;
@@ -19,26 +13,5 @@ public class MpiProperties extends ModuleProperties {
     public Integer getGlobalIdentifierDomainId() {
         String propertyName = RegistrationCoreConstants.GP_MPI_GLOBAL_IDENTIFIER_DOMAIN_ID;
         return getIntegerProperty(propertyName);
-    }
-
-    private Integer getIntegerProperty(String propertyName) {
-        String propertyValue = getProperty(propertyName);
-        try {
-            return Integer.valueOf(propertyValue);
-        } catch (NumberFormatException e) {
-            throw new APIException("Incorrect property value. " + propertyValue + " cannot be cast to Integer");
-        }
-    }
-
-    private String getProperty(String propertyName) {
-        if (MPI_PROPERTIES.get(propertyName) == null) {
-            initializeProperty(propertyName);
-        }
-        return MPI_PROPERTIES.get(propertyName);
-    }
-
-    private void initializeProperty(String propertyName) {
-        String propertyValue = readProperty(propertyName);
-        MPI_PROPERTIES.put(propertyName, propertyValue);
     }
 }
