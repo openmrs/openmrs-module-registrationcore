7a76a5906524828bd83c22e9bb79a3ac2b700dea
approce <seateq64@gmail.com>
2015-08-02 18:29:27 +0300

    Create WORKAROUND for "initialization". Should be changed by correct bean initialization.

--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/PatientIdentifierMapper.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/openempi/PatientIdentifierMapper.java
@@ -3,10 +3,7 @@ package org.openmrs.module.registrationcore.api.mpi.openempi;
 import javafx.util.Pair;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.GlobalProperty;
 import org.openmrs.api.APIException;
-import org.openmrs.api.AdministrationService;
-import org.openmrs.module.registrationcore.RegistrationCoreConstants;
 import org.openmrs.module.registrationcore.api.mpi.common.MpiProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -17,16 +14,42 @@ import java.util.List;
 public class PatientIdentifierMapper {
 
     private final Log log = LogFactory.getLog(this.getClass());
-
-    //first element ("key") - local, second element ("value") - mpi.
-    private static final List<Pair<Integer, Integer>> MAPPED_ID = new LinkedList<Pair<Integer, Integer>>();
     private static final String SPLITTER_SIGN = ":";
+    //first element ("key") - local, second element ("value") - mpi.
+    private static List<Pair<Integer, Integer>> MAPPED_ID;
 
     @Autowired
     @Qualifier("registrationcore.mpiProperties")
     private MpiProperties mpiProperties;
 
+    public Integer getMappedLocalIdentifierTypeId(Integer mpiIdentifierTypeId) {
+        validateInit();
+        for (Pair<Integer, Integer> pair : MAPPED_ID) {
+            if (pair.getValue().equals(mpiIdentifierTypeId))
+                return pair.getKey();
+        }
+        return null;
+    }
+
+    public Integer getMappedMpiIdentifierTypeId(Integer localIdentifierTypeId) {
+        validateInit();
+        for (Pair<Integer, Integer> pair : MAPPED_ID) {
+            if (pair.getKey().equals(localIdentifierTypeId))
+                return pair.getValue();
+        }
+        return null;
+    }
+
+    private void validateInit() {
+        if (MAPPED_ID == null) {
+            init();
+        }
+    }
+
+    //TODO refactor to perform initialization on refresh context.
     public void init() {
+        MAPPED_ID = new LinkedList<Pair<Integer, Integer>>();
+        log.error("start init method of PatientIdentifierMapper");
         List<String> properties = mpiProperties.getLocalMpiIdentifierTypeMap();
         for (String mappedIdentifiers : properties) {
             Pair<Integer, Integer> pair = parseIdentifiers(mappedIdentifiers);
@@ -60,21 +83,5 @@ public class PatientIdentifierMapper {
             throw new APIException("Can't create identifier pair for values: local= " +
                     localString + ", mpi=" + mpiString);
         }
-    }
-
-    public Integer getMappedLocalIdentifierTypeId(Integer mpiIdentifierTypeId) {
-        for (Pair<Integer, Integer> pair : MAPPED_ID) {
-            if (pair.getValue().equals(mpiIdentifierTypeId))
-                return pair.getKey();
-        }
-        return null;
-    }
-
-    public Integer getMappedMpiIdentifierTypeId(Integer localIdentifierTypeId) {
-        for (Pair<Integer, Integer> pair : MAPPED_ID) {
-            if (pair.getKey().equals(localIdentifierTypeId))
-                return pair.getValue();
-        }
-        return null;
     }
 }
