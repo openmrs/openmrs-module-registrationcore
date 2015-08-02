6701eb9de829f7edaca00d4f23230e37ddd7de72
approce <seateq64@gmail.com>
2015-08-02 20:36:56 +0300

    Refactor identifierBuilder. Remove method to get Identifier source. Responsibilities of this method is to generate and to create identifier.

--- api/src/main/java/org/openmrs/module/registrationcore/api/impl/IdentifierBuilder.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/impl/IdentifierBuilder.java
@@ -4,13 +4,11 @@ import org.openmrs.Location;
 import org.openmrs.PatientIdentifier;
 import org.openmrs.PatientIdentifierType;
 import org.openmrs.api.APIException;
-import org.openmrs.api.AdministrationService;
 import org.openmrs.api.LocationService;
 import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.idgen.IdentifierSource;
 import org.openmrs.module.idgen.service.IdentifierSourceService;
-import org.openmrs.module.registrationcore.RegistrationCoreConstants;
 import org.openmrs.validator.PatientIdentifierValidator;
 
 public class IdentifierBuilder {
@@ -19,41 +17,14 @@ public class IdentifierBuilder {
 
     private IdentifierSourceService iss;
 
-    private AdministrationService adminService;
-
     private PatientService patientService;
 
     public void setLocationService(LocationService locationService) {
         this.locationService = locationService;
     }
 
-    public void setAdminService(AdministrationService adminService) {
-        this.adminService = adminService;
-    }
-
     public void setPatientService(PatientService patientService) {
         this.patientService = patientService;
-    }
-
-    public Integer getOpenMrsIdentifierSourceId() {
-        String propertyName = RegistrationCoreConstants.GP_IDENTIFIER_SOURCE_ID;
-        return getProperty(propertyName);
-    }
-
-    public Integer getIdentifierIdByName(String identifierName) {
-        String propertyName = RegistrationCoreConstants.GP_IDENTIFIER_SOURCE_ID_COMMON + identifierName;
-        return getProperty(propertyName);
-    }
-
-    private Integer getProperty(String propertyName) {
-        String propertyValue = adminService.getGlobalProperty(propertyName);
-        Integer intPropertyValue;
-        try {
-            intPropertyValue = Integer.valueOf(propertyValue);
-        } catch (NumberFormatException e) {
-            throw new APIException("Identifier source id should be a number");
-        }
-        return intPropertyValue;
     }
 
     public PatientIdentifier generateIdentifier(Integer sourceId, Location location) {
@@ -72,17 +43,6 @@ public class IdentifierBuilder {
         return new PatientIdentifier(identifierValue, identifierType, location);
     }
 
-    private IdentifierSource getSource(Integer identifierId) {
-        IdentifierSource identifierSource = getIss().getIdentifierSource(identifierId);
-        validateIdentifierSource(identifierSource, identifierId);
-        return identifierSource;
-    }
-
-    private void validateIdentifierSource(IdentifierSource idSource, Integer identifierId) {
-        if (idSource == null)
-            throw new APIException("cannot find identifier source with id:" + identifierId);
-    }
-
     private Location getLocation(Location identifierLocation) {
         if (identifierLocation == null) {
             identifierLocation = locationService.getDefaultLocation();
@@ -91,15 +51,26 @@ public class IdentifierBuilder {
         return identifierLocation;
     }
 
+    private IdentifierSource getSource(Integer identifierId) {
+        IdentifierSource identifierSource = getIss().getIdentifierSource(identifierId);
+        validateIdentifierSource(identifierSource, identifierId);
+        return identifierSource;
+    }
+
+    private IdentifierSourceService getIss() {
+        if (iss == null) {
+            iss = Context.getService(IdentifierSourceService.class);
+        }
+        return iss;
+    }
+
     private void validateIdentifierLocation(Location identifierLocation) {
         if (identifierLocation == null)
             throw new APIException("Failed to resolve location to associate to patient identifiers");
     }
 
-    public IdentifierSourceService getIss() {
-        if (iss == null) {
-            iss = Context.getService(IdentifierSourceService.class);
-        }
-        return iss;
+    private void validateIdentifierSource(IdentifierSource idSource, Integer identifierId) {
+        if (idSource == null)
+            throw new APIException("cannot find identifier source with id:" + identifierId);
     }
 }
--- api/src/test/java/org/openmrs/module/registrationcore/api/impl/IdentifierBuilderTest.java
+++ api/src/test/java/org/openmrs/module/registrationcore/api/impl/IdentifierBuilderTest.java
@@ -9,29 +9,24 @@ import org.openmrs.Location;
 import org.openmrs.PatientIdentifier;
 import org.openmrs.PatientIdentifierType;
 import org.openmrs.api.APIException;
-import org.openmrs.api.AdministrationService;
 import org.openmrs.api.LocationService;
 import org.openmrs.api.PatientService;
 import org.openmrs.module.idgen.IdentifierSource;
 import org.openmrs.module.idgen.service.IdentifierSourceService;
-import org.openmrs.module.registrationcore.RegistrationCoreConstants;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
 public class IdentifierBuilderTest {
 
     private static final String OPENMRS_IDENTIFIER_SOURCE_ID = "1";
     private static final String OPENMRS_GENERATED_IDENTIFIER = "10012NF";
-    private static final String CUSTOM_MPI_IDENTIFIER_NAME = "ECID";
     private static final String CUSTOM_MPI_IDENTIFIER_ID = "2";
     private static final String CUSTOM_MPI_IDENTIFIER_VALUE = "16340061NF";
 
     @InjectMocks private IdentifierBuilder generator;
     @Mock private LocationService locationService;
     @Mock private IdentifierSourceService iss;
-    @Mock private AdministrationService adminService;
     @Mock private PatientService patientService;
 
     @Mock private Location defaultLocation;
@@ -43,40 +38,6 @@ public class IdentifierBuilderTest {
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-    }
-
-    @Test
-    public void testGetCorrectOpenMRSIdentifierSourceIdFromGP() throws Exception {
-        mockOpenMRSIdentifierSourceGP();
-
-        Integer actualId = generator.getOpenMrsIdentifierSourceId();
-        verify(adminService).getGlobalProperty(RegistrationCoreConstants.GP_IDENTIFIER_SOURCE_ID);
-        assertEquals(actualId, Integer.valueOf(OPENMRS_IDENTIFIER_SOURCE_ID));
-    }
-
-    @Test
-    public void testGetIdentifierIdByName() throws Exception {
-        mockCustomIdentifier();
-
-        Integer actualId = generator.getIdentifierIdByName(CUSTOM_MPI_IDENTIFIER_NAME);
-
-        verify(adminService).getGlobalProperty(
-                RegistrationCoreConstants.GP_IDENTIFIER_SOURCE_ID_COMMON + CUSTOM_MPI_IDENTIFIER_NAME);
-        assertEquals(actualId, Integer.valueOf(CUSTOM_MPI_IDENTIFIER_ID));
-    }
-
-    @Test(expected = APIException.class)
-    public void testGetIdentifierSourceThrowExceptionOnInvalidPropertyValue() throws Exception {
-        mockInvalidProperty();
-
-        generator.getOpenMrsIdentifierSourceId();
-    }
-
-    @Test(expected = APIException.class)
-    public void testGetIdentifierByNameThrowExceptionOnInvalidPropertyValue() throws Exception {
-        mockInvalidProperty();
-
-        generator.getIdentifierIdByName(CUSTOM_MPI_IDENTIFIER_NAME);
     }
 
     @Test
@@ -94,6 +55,20 @@ public class IdentifierBuilderTest {
         assertEquals(patientIdentifier.getIdentifier(), OPENMRS_GENERATED_IDENTIFIER);
         assertEquals(patientIdentifier.getIdentifierType(), identifierType);
         assertEquals(patientIdentifier.getLocation(), defaultLocation);
+    }
+
+    private void mockDefaultLocation(Location location) {
+        when(locationService.getDefaultLocation()).thenReturn(location);
+    }
+
+    private void mockIdentifierSource(IdentifierSource identifierSource) {
+        when(iss.getIdentifierSource(Integer.valueOf(OPENMRS_IDENTIFIER_SOURCE_ID))).thenReturn(identifierSource);
+        if (identifierSource != null)
+            when(identifierSource.getIdentifierType()).thenReturn(identifierType);
+    }
+
+    private void mockGeneratedIdentifier() {
+        when(iss.generateIdentifier(identifierSource, null)).thenReturn(OPENMRS_GENERATED_IDENTIFIER);
     }
 
     @Test
@@ -143,6 +118,11 @@ public class IdentifierBuilderTest {
         assertEquals(identifier.getLocation(), defaultLocation);
     }
 
+    private void mockPatientIdentifierType(PatientIdentifierType customMpiPatientIdentifierType) {
+        when(patientService.getPatientIdentifierType(Integer.valueOf(CUSTOM_MPI_IDENTIFIER_ID)))
+                .thenReturn(customMpiPatientIdentifierType);
+    }
+
     @Test
     public void testCreateIdentifierWithCustomLocation() throws Exception {
         mockDefaultLocation(defaultLocation);
@@ -163,39 +143,5 @@ public class IdentifierBuilderTest {
         mockDefaultLocation(null);
 
         generator.createIdentifier(Integer.valueOf(CUSTOM_MPI_IDENTIFIER_ID), CUSTOM_MPI_IDENTIFIER_VALUE, null);
-    }
-
-    private void mockOpenMRSIdentifierSourceGP() {
-        when(adminService.getGlobalProperty(RegistrationCoreConstants.GP_IDENTIFIER_SOURCE_ID))
-                .thenReturn(OPENMRS_IDENTIFIER_SOURCE_ID);
-    }
-
-    private void mockCustomIdentifier() {
-        when(adminService.getGlobalProperty(
-                RegistrationCoreConstants.GP_IDENTIFIER_SOURCE_ID_COMMON + CUSTOM_MPI_IDENTIFIER_NAME))
-                .thenReturn(CUSTOM_MPI_IDENTIFIER_ID);
-    }
-
-    private void mockInvalidProperty() {
-        when(adminService.getGlobalProperty(anyString())).thenReturn("not_numeric_value");
-    }
-
-    private void mockDefaultLocation(Location location) {
-        when(locationService.getDefaultLocation()).thenReturn(location);
-    }
-
-    private void mockIdentifierSource(IdentifierSource identifierSource) {
-        when(iss.getIdentifierSource(Integer.valueOf(OPENMRS_IDENTIFIER_SOURCE_ID))).thenReturn(identifierSource);
-        if (identifierSource != null)
-            when(identifierSource.getIdentifierType()).thenReturn(identifierType);
-    }
-
-    private void mockGeneratedIdentifier() {
-        when(iss.generateIdentifier(identifierSource, null)).thenReturn(OPENMRS_GENERATED_IDENTIFIER);
-    }
-
-    private void mockPatientIdentifierType(PatientIdentifierType customMpiPatientIdentifierType) {
-        when(patientService.getPatientIdentifierType(Integer.valueOf(CUSTOM_MPI_IDENTIFIER_ID)))
-                .thenReturn(customMpiPatientIdentifierType);
     }
 }
