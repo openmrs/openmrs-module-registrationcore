15029230c03b434bf825e4151e7ff165800d6070
approce <seateq64@gmail.com>
2015-08-19 11:15:45 +0300

    Create javadoc for MpiPatient Exporter/Importer/Updater.

--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/common/MpiPatientExporter.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/common/MpiPatientExporter.java
@@ -2,7 +2,14 @@ package org.openmrs.module.registrationcore.api.mpi.common;
 
 import org.openmrs.Patient;
 
+/**
+ * This service perform export patient to Mpi server.
+ */
 public interface MpiPatientExporter {
 
+    /**
+     * Perform export patient to MPI server
+     * @param patient patient which should be exported
+     */
     void exportPatient(Patient patient);
 }
--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/common/MpiPatientImporter.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/common/MpiPatientImporter.java
@@ -2,7 +2,15 @@ package org.openmrs.module.registrationcore.api.mpi.common;
 
 import org.openmrs.Patient;
 
+/**
+ * This service perform import patient from MPI server and convert it to local patient.
+ */
 public interface MpiPatientImporter {
 
+    /**
+     * Perform query to mpi server & convert it to local patient.
+     * @param patientId patient identifier
+     * @return converted patient
+     */
     Patient importMpiPatient(String patientId);
 }
