073e0dc87933cb7168528ec745c8445e03ec9a00
approce <seateq64@gmail.com>
2015-08-22 17:20:19 +0300

    Found bug in setting "UUID" of creator. Mark it as TODO.

--- api/src/main/java/org/openmrs/module/registrationcore/api/impl/RegistrationCoreServiceImpl.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/impl/RegistrationCoreServiceImpl.java
@@ -161,6 +161,8 @@ public class RegistrationCoreServiceImpl extends BaseOpenmrsService implements R
 
 		EventMessage eventMessage = new EventMessage();
 		eventMessage.put(RegistrationCoreConstants.KEY_PATIENT_UUID, patient.getUuid());
+
+		//TODO incorrect set of "UUID" each time it generates new RANDOM value, instead of set uuid of creator.
 		eventMessage.put(RegistrationCoreConstants.KEY_REGISTERER_UUID, patient.getCreator().getUuid());
 		eventMessage.put(RegistrationCoreConstants.KEY_DATE_REGISTERED, new SimpleDateFormat(
 		        RegistrationCoreConstants.DATE_FORMAT_STRING).format(patient.getDateCreated()));
