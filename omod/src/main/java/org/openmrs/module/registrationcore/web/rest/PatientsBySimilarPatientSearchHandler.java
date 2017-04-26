012dcd215f9101c726a8f7a85502af8be1e9e568
Rafal Korytkowski <rafal@openmrs.org>
2017-04-26 14:10:48 +0200

    RC-28 Give unique id for PatientsBySimilarPatientSearchHandler

--- omod/src/main/java/org/openmrs/module/registrationcore/web/rest/PatientsBySimilarPatientSearchHandler.java
+++ omod/src/main/java/org/openmrs/module/registrationcore/web/rest/PatientsBySimilarPatientSearchHandler.java
@@ -57,7 +57,7 @@ public class PatientsBySimilarPatientSearchHandler implements SearchHandler {
     private static final String COUNTRY = "country";
     private static final String STATE = "state";
 
-    private final SearchConfig searchConfig = new SearchConfig("default", RestConstants.VERSION_1 + "/patient",
+    private final SearchConfig searchConfig = new SearchConfig("registrationCoreFindSimilar", RestConstants.VERSION_1 + "/patient",
             Arrays.asList("1.8.*", "1.9.*", "1.10.*", "1.11.*", "1.12.*", "2.0.*", "2.1.*"),
             Arrays.asList(new SearchQuery.Builder("Allows you to find similar patients")
                     .withRequiredParameters(MATCH_SIMILAR)
