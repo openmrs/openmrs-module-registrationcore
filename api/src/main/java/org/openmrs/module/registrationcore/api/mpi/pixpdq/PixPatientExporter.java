e5c6b8f66c204f96dd2bdfa861a566e5842f8dfd
Paweł Gesek <pgesek@soldevelo.com>
2017-06-23 11:51:45 +0200

    Added patient export through PIX

--- api/src/main/java/org/openmrs/module/registrationcore/api/mpi/pixpdq/PixPatientExporter.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/mpi/pixpdq/PixPatientExporter.java
@@ -1,11 +1,18 @@
 package org.openmrs.module.registrationcore.api.mpi.pixpdq;
 
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.Message;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openmrs.Patient;
+import org.openmrs.module.registrationcore.api.mpi.common.MpiException;
 import org.openmrs.module.registrationcore.api.mpi.common.MpiPatientExporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 public class PixPatientExporter implements MpiPatientExporter {
+
+    private final Log log = LogFactory.getLog(this.getClass());
 
     @Autowired
     @Qualifier("registrationcore.mpiHl7v2Sender")
@@ -17,6 +24,23 @@ public class PixPatientExporter implements MpiPatientExporter {
 
     @Override
     public String exportPatient(Patient patient) {
-        throw new UnsupportedOperationException();
+        try {
+            Message admitMessage = pixPdqMessageUtil.createAdmit(patient);
+            Message response = hl7v2Sender.sendPdqMessage(admitMessage);
+
+            if (pixPdqMessageUtil.isQueryError(response)) {
+                throw new MpiException("Error querying patient data during export");
+            }
+
+            // TODO: return ID
+            return null;
+        } catch (Exception e) {
+            log.error(e);
+            if (e instanceof  MpiException) {
+                throw (MpiException) e;
+            } else {
+                throw new MpiException("Error while exporting patient", e);
+            }
+        }
     }
 }
