f655c0d04a200eeba6df9abb3331f7583f6edae7
Pawel Cieszko <pcieszko@soldevelo.com>
2018-04-12 14:44:05 +0200

    ITOS-9: removed undesired line

--- api/src/main/java/org/openmrs/module/registrationcore/api/xdssender/XdsCcdImporter.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/xdssender/XdsCcdImporter.java
@@ -1,6 +1,5 @@
 package org.openmrs.module.registrationcore.api.xdssender;
 
-
 import org.dcm4chee.xds2.common.exception.XDSException;
 import org.openmrs.Patient;
 import org.openmrs.module.xdssender.api.domain.Ccd;
