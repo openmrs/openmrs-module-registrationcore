8ead7b88e553cbf9780a7f5e642dd1365229924a
Daniel Serkowski <dserkowski@soldevelo.com>
2018-04-24 13:23:32 +0200

    ITOS-15: Added PIX error handling

--- api/src/main/java/org/openmrs/module/registrationcore/api/impl/PatientCreationListener.java
+++ api/src/main/java/org/openmrs/module/registrationcore/api/impl/PatientCreationListener.java
@@ -1,14 +1,19 @@
 package org.openmrs.module.registrationcore.api.impl;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.openmrs.Patient;
 import org.openmrs.PatientIdentifier;
 import org.openmrs.User;
 import org.openmrs.api.UserService;
 import org.openmrs.event.Event;
 import org.openmrs.module.registrationcore.RegistrationCoreConstants;
+import org.openmrs.module.registrationcore.api.errorhandling.ErrorHandlingService;
+import org.openmrs.module.registrationcore.api.mpi.common.MpiException;
 import org.openmrs.module.registrationcore.api.mpi.common.MpiProperties;
 
 import javax.jms.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class listen for patient creation event.
@@ -16,7 +21,9 @@ import javax.jms.Message;
  * and perform update for local patient with new one patient identifier.
  */
 public class PatientCreationListener extends PatientActionListener {
-
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(PatientCreationListener.class);
+    
     private MpiProperties mpiProperties;
 
     private IdentifierBuilder identifierBuilder;
@@ -51,9 +58,25 @@ public class PatientCreationListener extends PatientActionListener {
     @Override
     public void performMpiAction(Message message) {
         Patient patient = extractPatient(message);
-
-        String personId = pushPatientToMpiServer(patient);
-
+    
+        String personId = null;
+        try {
+            personId = pushPatientToMpiServer(patient);
+        } catch (Exception e) {
+            ErrorHandlingService errorHandler = coreProperties.getPixErrorHandlingService();
+            if (errorHandler == null) {
+                throw new MpiException("PIX patient push exception occurred "
+                        + "with not configured PIX error handler", e);
+            } else {
+                LOGGER.error("PIX patient push exception occurred", e);
+                errorHandler.handle(e.getMessage(),
+                        "org.openmrs.module.registrationcore.api.mpi.pixpdq.PixPatientExporter",
+                        true,
+                        ExceptionUtils.getFullStackTrace(e));
+            }
+    
+        }
+        
         if (personId != null) {
             updatePatient(patient, message, personId);
         }
